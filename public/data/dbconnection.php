ea2fcc7357138bc81076c11a2ba34ceb5508b8cc
Brian Ragusa <b.v.ragusa@gmail.com>
2024-12-08 16:20:35 -0500

    remove connection info

--- public/data/dbconnection.php
+++ public/data/dbconnection.php
@@ -1,10 +1,10 @@
 <?php require 'utility.php';?><?php
   header('Content-Type: application/json; charset=utf-8');
   
-  $dbhost = 'localhost';
-  $dbuser = 'southsho_admin';
-  $dbpass = 'SouthShoreWeb_Admin';
-  $dbname = 'southsho_medications';  
+  $dbhost = 'xxx';
+  $dbuser = 'xxx';
+  $dbpass = 'xxx';
+  $dbname = 'xxx';  
 
   try {
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
